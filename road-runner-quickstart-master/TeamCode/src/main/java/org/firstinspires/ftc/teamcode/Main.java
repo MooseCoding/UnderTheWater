a3b031ebe2809e710ec0c4cadb5654f7806670ce
jEdisonKnights-FTC <jedisonknights@gmail.com>
2024-05-25 14:31:04 -0400

    Init
    joe mama

---
+++ road-runner-quickstart-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.outoftheboxrobotics.photoncore.Photon;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Photon
+public class Main extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        while (!isStopRequested()) {
+
+        }
+    }
+}
