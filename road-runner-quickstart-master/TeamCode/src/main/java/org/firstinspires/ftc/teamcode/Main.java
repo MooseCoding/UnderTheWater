be66188f7e8b606d8d1f72f112015459517c570f
MooseCoding <113293872+MooseCoding@users.noreply.github.com>
2024-09-18 18:26:02 -0400

    Update SDK

--- road-runner-quickstart-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
+++ road-runner-quickstart-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
@@ -14,13 +14,15 @@ public class Main extends LinearOpMode {
             DcMotorEx fR = hardwareMap.dcMotor.get("frontRightMotor");
             DcMotorEx bR = hardwareMap.dcMotor.get("backRightMotor");
 
-            // Set Motors in Reverse
+            DcMotor m; 
 
             waitForStart();
 
-            if (isStopRequested()) {return;}
+            if (isStopRequested()) {
+                return;
+            }
 
-            while(opModeIsActive()) {
+            while (opModeIsActive()) {
                 double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
                 double rx = gamepad1.right_stick_x;
                 double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
@@ -28,7 +30,7 @@ public class Main extends LinearOpMode {
                 // This ensures all the powers maintain the same ratio,
                 // Denominator is the largest motor power (absolute value) or 1
                 // but only if at least one is out of the range [-1, 1]
-                double d = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1); // Denominator 
+                double d = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1); // Denominator
                 double fLP = (y + x + rx) / d; // Front left motor power
                 double bLP = (y - x + rx) / d; // Back left motor power
                 double fRP = (y - x - rx) / d; // Front right motor power
---
+++ road-runner-quickstart-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MechanumDrive.java
@@ -0,0 +1,5 @@
+
+
+public class MechanumDrive extends OpMode {
+    Dcmotor m = hardwareMap.get("hi");
+}
